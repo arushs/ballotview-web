12a69c24da9f04490eedf4f29463f22c2513fcf7
Andrew Jiang <andrewji@usc.edu>
2016-10-19 01:47:50 -0700

    modified candidates json

--- src/server/controllers/googlecivic.js
+++ src/server/controllers/googlecivic.js
@@ -9,29 +9,32 @@ const parties = {
   Green: '#558B2F'
 };
 
+function capitalizeEachWord(str) {
+  return str.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+
 function BuildReferendumObject(contest)
 {
     data = {};
     data['title'] = [contest.referendumTitle + ": "];
-    if ("referendumSubtitle" in contest)
-    {
-        data['title'].push(contest.referendumSubtitle);
-    }
-    else
-    {
-        data['title'].push(contest.referendumBrief);
+    if ('referendumSubtitle' in contest) {
+      data['title'].push(contest.referendumSubtitle);
+    } else if ('referendumBrief' in contest) {
+      data['title'].push(contest.referendumBrief);
     }
     data['subtext'] = [contest.referendumText];
 
     data['poll'] = [];
     var subPoll = {};
     subPoll['info'] = [];
-    subPoll['info'].push({"title":["Yes"]});
+    subPoll['info'].push({ title: ['Yes'] });
     data['poll'].push(subPoll);
 
     subPoll = {};
     subPoll['info'] = [];
-    subPoll['info'].push({"title":["No"]});
+    subPoll['info'].push({ title: ['No'] });
     data['poll'].push(subPoll);
     return data;
 }
@@ -43,24 +46,24 @@ function BuildCandidateObject(contest)
     data['subtext'] = ["Vote for " + contest.numberVotingFor];
     data['poll'] = [];
 
-    for (var i in contest.candidates)
-    {
-        candidate = contest.candidates[i];
-        var subPoll = {};
-        subPoll['info'] = [];
-        subPoll['info'].push({
-            "title":[candidate.name]
-        });
-        subPoll.info.trail = [candidate.party];
-        for (var key in parties)
-        {
-            // if (candidate.party.includes(key))
-            // {
-            //     subPoll.info['color'] = parties[key];
-            // }
-        }
+    for (var candidate of contest.candidates) {
+      console.log(candidate);
+      var subPoll = {};
 
-        data['poll'].push(subPoll);
+      subPoll.info = candidate.name.split(/, |\//).map(function (name) {
+        name = name.split(' for ');
+        var infoObj = { title: [capitalizeEachWord(name[0])] };
+        if (1 in name) infoObj.sub = ['for ' + name[1]];
+        return infoObj;
+      });
+      subPoll.info.trail = [candidate.party];
+      for (var key in parties) {
+        if ('party' in candidate && candidate.party.includes(key)) {
+          subPoll.info['color'] = parties[key];
+        }
+      }
+
+      data['poll'].push(subPoll);
     }
 
     return data;
@@ -69,45 +72,44 @@ function BuildCandidateObject(contest)
 router.get('/', function(req, res) {
 	// var address = {};
 	var info = request({
-	uri:'https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyChX3BTs57b15Q-rTx2nxwhazzJ4jpi2xQ&address=2365+Scarff+Street%2C+Los+Angeles+90007',
-	method: 'get',
-	json: true,
-   }, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-
-    } else {
-      console.log(error);
-    }
+    uri:'https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyChX3BTs57b15Q-rTx2nxwhazzJ4jpi2xQ&address=2365+Scarff+Street%2C+Los+Angeles+90007',
+    method: 'get',
+    json: true,
+  }, (error, response, body) => {
 
     var resp = {};
-    resp['ballot'] = [];
 
-    var stateMeasureResp = {};
-    stateMeasureResp['title'] = "State Measures";
-    stateMeasureResp['cards'] = [];
+    if (error || response.statusCode !== 200) {
+      console.log(error);
+    } else {
+      resp['ballot'] = [];
 
-    var candidateResp = {};
-    candidateResp.title = "Candidates";
-    candidateResp.cards = [];
+      var stateMeasureResp = {};
+      stateMeasureResp['title'] = "State Measures";
+      stateMeasureResp['cards'] = [];
 
-    for(var i in response.body.contests){
+      var candidateResp = {};
+      candidateResp.title = "Candidates";
+      candidateResp.cards = [];
+
+      for(var i in response.body.contests){
         var contest = response.body.contests[i];
-        if (contest.type == 'Referendum')
-        {
-            var data = BuildReferendumObject(contest)
-            stateMeasureResp['cards'].push(data);
+        if (contest.type == 'Referendum') {
+          var data = BuildReferendumObject(contest)
+          stateMeasureResp['cards'].push(data);
         }
-        else if (contest.type == "General")
-        {
-            var data = BuildCandidateObject(contest);
-            candidateResp['cards'].push(data);
-            // console.log(contest);
+        else if (contest.type == "General") {
+          var data = BuildCandidateObject(contest);
+          candidateResp['cards'].push(data);
+          // console.log(contest);
         }
+      }
+
+      resp['ballot'].push(candidateResp);
+      resp['ballot'].push(stateMeasureResp);
+      // console.log(candidateResp);
     }
 
-    resp['ballot'].push(candidateResp);
-    resp['ballot'].push(stateMeasureResp);
-    console.log(candidateResp);
     res.json(resp);
   });
 });
